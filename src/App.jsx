e7f5e890ae83818f9567bafe4f58af375bca21cf
Agnibha Chakraborty <agnichak@amazon.com>
2024-07-10 19:49:22 +0530

    Display projects in the sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -22,12 +22,14 @@ function App() {
       };
       return {
         ...prevState,
+        selectedProjectId: undefined,
         projects: [...prevState.projects, newProject],
       };
     });
   }
-  console.log(projectsState);
+
   let content;
+
   if (projectsState.selectedProjectId === null) {
     content = <NewProject onAdd={handleAddProject} />;
   } else if (projectsState.selectedProjectId === undefined) {
@@ -35,7 +37,10 @@ function App() {
   }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
